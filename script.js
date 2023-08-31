e30272a9cfc6eb920a629a7e08efc4a1d36d8b85
Ankit Litouriya <118179332+Ankitlitouriya@users.noreply.github.com>
2023-09-01 00:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ const button = documnet.querySelector("form input[type ='button]'")
+const option = document.querySelector("#colorSelect");
+button.onclick()=>{
+	const value = option.value;
+	option.queryselectorAll("option").foreach(e=>{
+		 if (e.textContent.trim()==value) {
+		 	option.removeChild(e);
+		 }
+	} )
+}
