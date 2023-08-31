ad2dd4e72d72d95dc0b9976030af891f973c8bde
Ankit Litouriya <118179332+Ankitlitouriya@users.noreply.github.com>
2023-09-01 00:44:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 //your JS code here. If required.
- const button = documnet.querySelector("form input[type ='button]'")
-const option = document.querySelector("#colorSelect");
-button.onclick()=>{
-	const value = option.value;
-	option.queryselectorAll("option").foreach(e=>{
-		 if (e.textContent.trim()==value) {
-		 	option.removeChild(e);
-		 }
-	} )
+//  const button = documnet.querySelector("form input[type ='button]'")
+// const option = document.querySelector("#colorSelect");
+// button.onclick()=>{
+// 	const value = option.value;
+// 	option.queryselectorAll("option").foreach(e=>{
+// 		 if (e.textContent.trim()==value) {
+// 		 	option.removeChild(e);
+// 		 }
+// 	} )
+// }
+
+
+const button = document.querySelector("form input[type='button']");
+const options = document.querySelector("#colorSelect");
+
+button.onclick = () => {
+	// document.body.append(document.createTextNode(options.value));
+	const value = options.value;
+	document.querySelectorAll("option").forEach(e => {
+		if (e.textContent.trim() == value) {
+			options.removeChild(e);
+		}
+	})
 }
